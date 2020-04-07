c826746cb654e5fcbd124bb946bdadd49b53845a
JanHovland <jan.hovland@lyse.net>
2020-04-07 08:08:56 +0200

    Update

--- SafariView/ContentView.swift
+++ SafariView/ContentView.swift
@@ -7,15 +7,40 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct ContentView: View {
+    // whether or not to show the Safari ViewController
+    @State var showSafari = false
+    // initial URL string
+    @State var urlString = "https://duckduckgo.com"
+
     var body: some View {
-        Text("Hello, World!")
+        Button(action: {
+            // update the URL if you'd like to
+            self.urlString = "https://duckduckgo.com"
+            // tell the app that we want to show the Safari VC
+            self.showSafari = true
+        }) {
+            Text("Present Safari")
+        }
+        // summon the Safari sheet
+        .sheet(isPresented: $showSafari) {
+            SafariView(url:URL(string: self.urlString)!)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct SafariView: UIViewControllerRepresentable {
+
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
     }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+
+    }
+
 }
